d617fba36c8963bf8de736bd6e872ea7bf7ec7af
Allexsen <asvanidze12@gmail.com>
2024-09-16 13:13:21 +0200

    pathfix

--- run_asr_server_win.bat
+++ run_asr_server_win.bat
@@ -1,5 +1,6 @@
-python sherpa-onnx\python-api-examples\streaming_server.py ^
-  --encoder models\sherpa-onnx-streaming-zipformer-en-2023-06-26\encoder-epoch-99-avg-1-chunk-16-left-128.onnx ^
-  --decoder models\sherpa-onnx-streaming-zipformer-en-2023-06-26\decoder-epoch-99-avg-1-chunk-16-left-128.onnx ^
-  --joiner models\sherpa-onnx-streaming-zipformer-en-2023-06-26\joiner-epoch-99-avg-1-chunk-16-left-128.onnx ^
-  --tokens models\sherpa-onnx-streaming-zipformer-en-2023-06-26\tokens.txt
+cd sherpa-onnx
+python python-api-examples\streaming_server.py ^
+  --encoder .\models\sherpa-onnx-streaming-zipformer-en-2023-06-26\encoder-epoch-99-avg-1-chunk-16-left-128.onnx ^
+  --decoder .\models\sherpa-onnx-streaming-zipformer-en-2023-06-26\decoder-epoch-99-avg-1-chunk-16-left-128.onnx ^
+  --joiner .\models\sherpa-onnx-streaming-zipformer-en-2023-06-26\joiner-epoch-99-avg-1-chunk-16-left-128.onnx ^
+  --tokens .\models\sherpa-onnx-streaming-zipformer-en-2023-06-26\tokens.txt
